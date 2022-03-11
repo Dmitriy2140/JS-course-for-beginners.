a3119e3ceb0990adfd21ed2cc80515a1ac9dc054
Dmitriy2140 <dimon_shkurat@mail.ru>
2022-03-11 09:48:19 +0300

    Обьекты. Сокращенный формат записи свойств.

---
+++ object.js
@@ -0,0 +1,12 @@
+//Обьекты. Сокращенный формат записи свойств.
+
+const name = 'Dima'
+const postsQty = 30
+
+const userProfile = {
+    name,
+    postsQty,
+    hasSihnedAgreement: false
+}
+
+console.log(userProfile)
