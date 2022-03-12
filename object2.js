35491a99a8a8305d649daf1c3bb665cca386564e
Dmitriy2140 <dimon_shkurat@mail.ru>
2022-03-12 11:16:42 +0300

    Добавление функции в объект

---
+++ object2.js
@@ -0,0 +1,9 @@
+//добавление функции в объект
+const myCity = {
+    city: 'New York',
+    cityGreeting: function () {
+        console.log('Greetings!!')
+    }
+}
+undefined
+myCity.cityGreeting()
